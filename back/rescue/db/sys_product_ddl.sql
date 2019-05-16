07fd44c184775f9c6780aaac8c6a6e244fa0bdc1
chengjd <chengjunda123@126.com>
2019-05-16 16:08:23 +0800

    数据库脚本修改

--- back/rescue/db/sys_product_ddl.sql
+++ back/rescue/db/sys_product_ddl.sql
@@ -135,6 +135,20 @@ CREATE TABLE IF NOT EXISTS `user_team` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='用户战队关系表';
 
+CREATE TABLE IF NOT EXISTS `task_process_settings` (
+  `id` varchar(50) NOT NULL,
+  `task_name` varchar(50) NOT NULL,
+  `task_id` varchar(50) NOT NULL,
+  `plan_id` varchar(50) NOT NULL,
+  `plan_name` varchar(50) NOT NULL,
+  `task_process_id` varchar(50) NOT NULL,
+  `task_process_name` varchar(50) NOT NULL,
+  `end_notice_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `need_notice` varchar(2) NOT NULL DEFAULT '1' COMMENT '1 需要提醒   0 不需要提醒',
+  `create_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `modify_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT=' ';
 -- 数据导出被取消选择。
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
